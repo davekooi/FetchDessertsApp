09cf8e22f64e4f51d8e29046168fc18c1959b5a6
davekooi <dkooi@umich.edu>
2024-06-07 18:54:59 -0400

    Initial Commit

---
+++ FetchDessertCodingChallenge/FetchDessertCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertCodingChallengeApp.swift
+//  FetchDessertCodingChallenge
+//
+//  Created by David Kooistra on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
